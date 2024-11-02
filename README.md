1162fdaf19f9bde4655afa5a82daa40ca29091c6
Agnė Vabalaitė <126966564+Agne-Vab@users.noreply.github.com>
2024-11-02 13:12:44 +0200

    Adapted task to be ready for GitHub

---
+++ README.md
@@ -0,0 +1,58 @@
+# Weekly Retention Cohorts Analysis for Subscription Churn
+
+## Introduction
+
+This repository contains a focused analysis of weekly subscription retention cohorts. The objective was to examine subscription churn using SQL and basic cohort visualization. This project emphasizes SQL proficiency and cohort analysis skills, using data extracted from BigQuery and visualized in Google Sheets.
+
+Since this was a learning task, it’s scoped to be a quick analysis rather than a comprehensive project.
+
+## Dataset
+
+This project was developed using a proprietary dataset provided for educational purposes, which is not included in the repository due to restrictions. Redistribution or commercial use of the project contents is not permitted.
+
+Below is a description of the dataset structure:
+
+- user_pseudo_id.
+- category.
+- country.
+- subscription_start.
+- subscription_end.
+
+If you'd like to replicate the analysis, you may substitute with a similar dataset.
+
+## Objectives
+
+1. Analyze weekly retention and churn rates for subscribers.
+2. Provide retention insights by week, focusing on patterns across a 6-week period.
+
+## Tech Stack
+
+- **SQL** (BigQuery) – for data extraction and cohort calculations.
+- **Google Sheets** – for creating quick visualizations of cohort retention data.
+
+## Key Findings
+
+- Most subscriptions were started a couple of weeks before the Christmas period (starting 2020-12-06).
+- Retention rates peaked for subscriptions started between late December and early January, with these subscribers showing the highest engagement in the following weeks.
+- Retention began to decline in early January, suggesting a seasonal effect on subscription commitment.
+- Even though most new subscriptions started just before the Christmas period, the ones who stayed longest were the ones who became subscribers during the main holiday weeks (2020-12-20 and 2020-12-27).
+
+## Recommendations
+
+- **Focus Marketing Efforts**: increase retention efforts or engagement campaigns for subscribers joining during high-churn periods, such as after the holidays.
+- **Analyze Further**: consider extending the cohort analysis beyond 6 weeks to understand longer-term retention behavior.
+
+## Possible Analysis Improvements
+
+To enhance this project in the future, the following could be added:
+- Optimizing the code and shortening it as cohorts and retention can be calculated using various approaches.
+- Incorporating more variables to analyze different drivers of retention (e.g., device type, demographics).
+
+## How to Use This Repository
+
+1. **SQL query**: The SQL file contains the code used to extract the cohort data from BigQuery.
+2. **images**: visualizations are provided in the screenshots.
+
+## Contact
+LinkedIn - [Agnė Vabalaitė](www.linkedin.com/in/agnė-vabalaitė).
+Gmail - vab.agne@gmail.com
---
+++ SQL query.sql
@@ -0,0 +1,146 @@
+-- Churn with sub numbers
+
+WITH
+  sub AS (
+  SELECT
+    user_pseudo_id,
+    subscription_start,
+    subscription_end,
+    DATE_DIFF(subscription_end, subscription_start, WEEK) AS Sub_lenght_weeks,
+    DATE_TRUNC(subscription_start, WEEK) start_week_date
+  FROM
+    `tc-da-1.turing_data_analytics.subscriptions`)
+
+
+SELECT
+  sub.start_week_date,
+  (SELECT COUNT(user_pseudo_id), FROM sub s WHERE sub.start_week_date = s.start_week_date) AS num_at_start,
+  w1.week_1,
+  w2.week_2,
+  w3.week_3,
+  w4.week_4,
+  w5.week_5,
+  w6.week_6
+FROM
+  sub
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_1
+    FROM sub
+    WHERE (Sub_lenght_weeks > 0 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 13) as w1
+ON w1.start_week_date = sub.start_week_date
+
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_2
+    FROM sub
+    WHERE (Sub_lenght_weeks > 1 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 12) as w2
+ON w2.start_week_date = sub.start_week_date
+
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_3
+    FROM sub
+    WHERE (Sub_lenght_weeks > 2 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 11) as w3
+ON w3.start_week_date = sub.start_week_date
+
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_4
+    FROM sub
+    WHERE (Sub_lenght_weeks > 3 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 10) as w4
+ON w4.start_week_date = sub.start_week_date
+
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_5
+    FROM sub
+    WHERE (Sub_lenght_weeks > 4 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 9) as w5
+ON w5.start_week_date = sub.start_week_date
+
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_6
+    FROM sub
+    WHERE (Sub_lenght_weeks > 5 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 8) as w6
+ON w6.start_week_date = sub.start_week_date
+
+GROUP BY
+  sub.start_week_date,w1.week_1, w2.week_2, w3.week_3, w4.week_4,w5.week_5, w6.week_6
+ORDER BY sub.start_week_date
+  
+  
+
+
+-- Churn with sub %
+
+
+WITH
+  sub AS (
+  SELECT
+    user_pseudo_id,
+    subscription_start,
+    subscription_end,
+    DATE_DIFF(subscription_end, subscription_start, WEEK) AS Sub_lenght_weeks,
+    DATE_TRUNC(subscription_start, WEEK) start_week_date
+  FROM
+    `tc-da-1.turing_data_analytics.subscriptions`)
+
+SELECT
+  sub.start_week_date,
+  (SELECT COUNT(user_pseudo_id), FROM sub s WHERE sub.start_week_date = s.start_week_date) AS num_at_start,
+  CONCAT(ROUND((w1.week_1/COUNT(user_pseudo_id))*100,2),"%") as week_1,
+  CONCAT(ROUND((w2.week_2/COUNT(user_pseudo_id))*100,2),"%") as week_2,
+  CONCAT(ROUND((w3.week_3/COUNT(user_pseudo_id))*100,2),"%") as week_3,
+  CONCAT(ROUND((w4.week_4/COUNT(user_pseudo_id))*100,2),"%") as week_4,
+  CONCAT(ROUND((w5.week_5/COUNT(user_pseudo_id))*100,2),"%") as week_5,
+  CONCAT(ROUND((w6.week_6/COUNT(user_pseudo_id))*100,2),"%") as week_6
+FROM
+  sub
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_1
+    FROM sub
+    WHERE (Sub_lenght_weeks > 0 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 13) as w1
+ON w1.start_week_date = sub.start_week_date
+
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_2
+    FROM sub
+    WHERE (Sub_lenght_weeks > 1 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 12) as w2
+ON w2.start_week_date = sub.start_week_date
+
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_3
+    FROM sub
+    WHERE (Sub_lenght_weeks > 2 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 11) as w3
+ON w3.start_week_date = sub.start_week_date
+
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_4
+    FROM sub
+    WHERE (Sub_lenght_weeks > 3 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 10) as w4
+ON w4.start_week_date = sub.start_week_date
+
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_5
+    FROM sub
+    WHERE (Sub_lenght_weeks > 4 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 9) as w5
+ON w5.start_week_date = sub.start_week_date
+
+FULL JOIN
+  (SELECT start_week_date, COUNT(user_pseudo_id) week_6
+    FROM sub
+    WHERE (Sub_lenght_weeks > 5 OR Sub_lenght_weeks IS NULL)
+    GROUP BY start_week_date limit 8) as w6
+ON w6.start_week_date = sub.start_week_date
+
+GROUP BY
+  sub.start_week_date,w1.week_1, w2.week_2, w3.week_3, w4.week_4,w5.week_5, w6.week_6
+ORDER BY sub.start_week_date
+  
+  
